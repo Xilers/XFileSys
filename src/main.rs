a6134162018aa580b2a11538b66343da689b8e45
KY00KIM <dev.kyoomin@gmail.com>
2024-06-24 03:45:59 +0900

    Add threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 mod file_io;
+mod threadpool;
 
 use file_io::{copy_part, create_file, read_file};
 use std::fs::File;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
-use std::thread::{self, JoinHandle};
 use std::time;
 
 /// @@ TODO:
 ///  - Add/Impl threadpool for preallocating threads
 ///
 fn main() {
+    benchmark_file_io_perf();
+}
+
+fn benchmark_file_io_perf() {
     const SRC_NAME: &str = "large_file_src.txt";
     const DEST_NAME: &str = "large_file_destination.txt";
-    const SRC_SIZE: u64 = 1024 * 1024 * 1024; // 1GB (in bytes)
+    const SRC_SIZE: u64 = 1024 * 1024 * 1024 * 4; // 4GB (in bytes)
     const THREAD_NUM: usize = 1;
+    let threadpool = threadpool::ThreadPool::new(THREAD_NUM);
 
     println!(
         "Writing file of size {} MB with {} Threads...",
@@ -34,7 +39,6 @@ fn main() {
     let dest_file = File::create(&dest_path).unwrap();
     let dest_file = Arc::new(Mutex::new(dest_file));
 
-    let mut handles: Vec<JoinHandle<()>> = vec![];
     for i in 0..THREAD_NUM {
         let offset = SRC_SIZE / THREAD_NUM as u64;
         let dest_file_clone = Arc::clone(&dest_file);
@@ -44,17 +48,14 @@ fn main() {
         } else {
             offset
         };
-        let handle = thread::spawn(move || {
+        threadpool.execute(move || {
             copy_part(&src_path, dest_file_clone, start, length).expect("Failed to copy part");
         });
-        handles.push(handle);
     }
-    for handle in handles {
-        handle.join().expect("Thread panicked!");
-    }
+    drop(threadpool);
     println!("Elapsed time: {} msec", current_time.elapsed().as_millis());
 
-    let src = read_file(SRC_NAME).unwrap();
-    let dest = read_file(DEST_NAME).unwrap();
-    println!("src length: {} B\ndest length: {} B", src.len(), dest.len());
+    // let src = read_file(SRC_NAME).unwrap();
+    // let dest = read_file(DEST_NAME).unwrap();
+    // println!("src length: {} B\ndest length: {} B", src.len(), dest.len());
 }
---
+++ src/threadpool.rs
@@ -0,0 +1,130 @@
+use core::marker::Send;
+use core::ops::FnOnce;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// # Arguments
+    ///
+    /// * `size` - The number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    /// Execute a function on the thread pool.
+    ///
+    /// # Arguments
+    ///
+    /// * `f` - The function to execute.
+    ///
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+
+    /// WIP :
+    /// Wait for all threads to finish.
+    pub fn join(&mut self) {
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // println!("Sending terminate message to all workers.");
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+        println!("Waiting for all workers to shut down...");
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    /// Create a new Worker.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The id of the worker.
+    /// * `receiver` - The receiver end of the channel, message from ThreadPool.
+    ///
+    /// # Returns
+    /// new Worker.
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let msg = receiver.lock().unwrap().recv().unwrap();
+
+            match msg {
+                Message::NewJob(job) => {
+                    // println!("Worker {} got a job; executing...", id);
+                    job.call_box();
+                }
+                Message::Terminate => {
+                    // println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
+        });
+        Worker {
+            id: id,
+            thread: Some(thread),
+        }
+    }
+}
